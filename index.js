05632738f38cd582a455b8fe7e0d4dd0839d3ac7
crissesoto <soto.crisse@hotmail.com>
2019-11-04 16:44:35 +0100

    added switch statement for keydown

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // quering for all the buttons
 const buttons = document.querySelectorAll(".drum");
 const letterW = document.querySelector(".w");
+const body = document.querySelector("body");
 
 // add eventListener to all buttons
 buttons.forEach(button => {
@@ -42,7 +43,45 @@ buttons.forEach(button => {
         sound.play();
         break;
       default:
+        alert("This key has not sound");
         break;
     }
   });
+});
+
+body.addEventListener("keydown", e => {
+  //alert("key pressed");
+  switch (e.key) {
+    case "w":
+      sound = new Audio("sounds/crash.mp3");
+      sound.play();
+      break;
+    case "a":
+      sound = new Audio("sounds/kick-bass.mp3");
+      sound.play();
+      break;
+    case "s":
+      sound = new Audio("sounds/snare.mp3");
+      sound.play();
+      break;
+    case "d":
+      sound = new Audio("sounds/tom-1.mp3");
+      sound.play();
+      break;
+    case "j":
+      sound = new Audio("sounds/tom-2.mp3");
+      sound.play();
+      break;
+    case "k":
+      sound = new Audio("sounds/tom-3.mp3");
+      sound.play();
+      break;
+    case "l":
+      sound = new Audio("sounds/tom-4.mp3");
+      sound.play();
+      break;
+    default:
+      alert("This key has not sound");
+      break;
+  }
 });
