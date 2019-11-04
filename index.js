3684c198ad6431da0f43f2a84da895904d48893b
crissesoto <soto.crisse@hotmail.com>
2019-11-04 14:06:34 +0100

    added switch statement to play audio

--- index.js
+++ index.js
@@ -6,11 +6,43 @@ const letterW = document.querySelector(".w");
 buttons.forEach(button => {
   button.addEventListener("click", () => {
     button.classList.toggle("pressed");
-  });
-});
+    button.style.color = "white";
+    let buttonInnerHTML = button.innerHTML;
 
-// add sound to each letter
-letterW.addEventListener("click", () => {
-  let sound = new Audio("sounds/tom-1.mp3");
-  sound.play();
+    let sound;
+
+    // switch statement
+    switch (buttonInnerHTML) {
+      case "w":
+        sound = new Audio("sounds/crash.mp3");
+        sound.play();
+        break;
+      case "a":
+        sound = new Audio("sounds/kick-bass.mp3");
+        sound.play();
+        break;
+      case "s":
+        sound = new Audio("sounds/snare.mp3");
+        sound.play();
+        break;
+      case "d":
+        sound = new Audio("sounds/tom-1.mp3");
+        sound.play();
+        break;
+      case "j":
+        sound = new Audio("sounds/tom-2.mp3");
+        sound.play();
+        break;
+      case "k":
+        sound = new Audio("sounds/tom-3.mp3");
+        sound.play();
+        break;
+      case "l":
+        sound = new Audio("sounds/tom-4.mp3");
+        sound.play();
+        break;
+      default:
+        break;
+    }
+  });
 });
