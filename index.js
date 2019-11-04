1a1fece2660ab4e915356679950cf7580aa92132
crissesoto <soto.crisse@hotmail.com>
2019-11-04 10:57:38 +0100

    added background images to the buttons

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // quering for all the buttons
 const buttons = document.querySelectorAll(".drum");
+const letterW = document.querySelector(".w");
 
-// add eventListener
+// add eventListener to all buttons
 buttons.forEach(button => {
   button.addEventListener("click", () => {
-    alert("I got clicked!");
+    button.classList.toggle("pressed");
   });
+});
+
+// add sound to each letter
+letterW.addEventListener("click", () => {
+  let sound = new Audio("sounds/tom-1.mp3");
+  sound.play();
 });
